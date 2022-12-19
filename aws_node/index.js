d5efa73b0fd55fb2da36ee84ce051e840d6de89e
paulpfleg <98637172+paulpfleg@users.noreply.github.com>
2022-12-19 22:39:05 +0100

    code beauty and new stylesheet

--- aws_node/index.js
+++ aws_node/index.js
@@ -17,7 +17,7 @@ app.use(logger)
 app.set('views', path.join(__dirname, 'public'));
 
 app.use(express.json())
-//app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
 
 app.use(bodyParser.urlencoded({
     extended:true
