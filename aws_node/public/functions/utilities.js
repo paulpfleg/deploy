b1c6954ee852a07687ec3d3b2354cb706ca3fa24
paulpfleg <98637172+paulpfleg@users.noreply.github.com>
2022-12-20 11:29:23 +0100

    consolidated css files

---
+++ aws_node/public/functions/utilities.js
@@ -0,0 +1,10 @@
+function shorten (item)
+{
+    item.lastModified=item.lastModified.slice(0,-8).replace(/-/g, ".").replace(/T/g, " ");
+    var len = item.key.length
+    item.shortkey=item.key
+    if (len>19) {
+      item.shortkey="..."+item.key.slice(-19)
+    }
+    return(item)
+}
